c0576e0c24ec39b9b6806cfb7f3a6e18b4fda5f2
Ben Price <ben@xyleth.co.uk>
2025-05-08 15:10:51 +0100

    Chaging the page title to be the HNWA logo

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "HNWA Notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
