37301b71e021fb7433c3fcdd37f293e1f74a84fc
Ben Price <ben@xyleth.co.uk>
2025-05-08 15:06:32 +0100

    fixing some linting issues

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,7 +4,7 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [Component.YourLogo()],
+  header: [Component.YourLogo(undefined)],
   afterBody: [],
   footer: Component.Footer({
     links: {
--- quartz/components/YourLogo.tsx
+++ quartz/components/YourLogo.tsx
@@ -1,22 +1,18 @@
 // In quartz/components/YourLogo.tsx
-import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { QuartzComponentConstructor } from "./types"
 
-const YourLogo: QuartzComponentConstructor = () => {
-  function Component(props: QuartzComponentProps) {
-    return (
-      <div className="your-logo">
-        <a href="/">
-          {/* For image logo */}
-          <img src="/static/logo.png" alt="HNWA Logo" />
-          <span className="logo-text">Holywell Neighbourhood Watch Association</span>
-          {/* Or for text logo */}
-          {/* <span className="logo-text">HNWA Notes</span> */}
-        </a>
-      </div>
-    )
-  }
-  
-  return Component
+const YourLogo: QuartzComponentConstructor = (opts?: undefined) => {
+  return () => (
+    <div className="your-logo">
+      <a href="/">
+        {/* For image logo */}
+        <img src="/static/logo.png" alt="HNWA Logo" />
+        <span className="logo-text">Holywell Neighbourhood Watch Association</span>
+        {/* Or for text logo */}
+        {/* <span className="logo-text">HNWA Notes</span> */}
+      </a>
+    </div>
+  )
 }
 
 export default YourLogo
